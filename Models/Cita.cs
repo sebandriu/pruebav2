82bc3e3c5b128deb79dc24fe6ef9563dbe601d6e
sebandriu <sebaandresrobles@live.cl>
2023-06-06 03:19:37 -0400

    Actualizacion Nº3
    Se hizo cambios en CSS y Iconos, ademas de cambiar algunos errores en el codigo fuente

--- Models/Cita.cs
+++ Models/Cita.cs
@@ -8,7 +8,7 @@ namespace demo.Models
         public long Id { get; set; }
         public string? Nombre { get; set; }
         public string? Descripcion { get; set; }
-        public byte[]? Fechacita { get; set; }
+        public DateTime Fechacita { get; set; }
         public long? DestinatariosId { get; set; }
 
         public virtual Destinatario? Destinatarios { get; set; }
